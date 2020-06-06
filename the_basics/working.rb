e5a585b47ebbf8b4920701853fc4b95a8f70dad0
Charles Keenan <Keenan.Charlese@gmail.com>
2020-06-06 16:32:23 -0300

    All done with these exercises

--- the_basics/working.rb
+++ the_basics/working.rb
@@ -10,14 +10,22 @@ puts 1234 / 100 % 10
 puts 1234 / 10 % 10
 puts 1234 % 10
 
+# Exercise 3, movie hash
 movies = {"Fight Club": 1996, "Pulp Fiction": 1991, "Unforgiven": 1992}
 
 movies.each { |k,v| puts v }
 
 puts movies[:"Fight Club"]
 
+# Exercise 4, create date array
 date_array = []
 
 movies.each { |k,v| date_array += [v] }
 
 puts date_array
+
+#factorial of 8
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+# square of random float
+puts 4.567**2
