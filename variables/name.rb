4a8827efddbc5c031a68c596201e1183e8f5fadd
Charles Keenan <Keenan.Charlese@gmail.com>
2020-06-08 11:03:04 -0300

    Name program repeats name over and over using times method.

--- variables/name.rb
+++ variables/name.rb
@@ -2,3 +2,7 @@ puts "Hello, I am Mac. What's your name?"
 name = gets.chomp
 
 puts "Hello #{name}, it's nice to meet you..."
+
+puts "I really like your name #{name}, I just want to say it over and over and over and over...."
+10.times { print "#{name}" }
+puts "\n There....that's better #{name}."
--- variables/variables.rb
+++ variables/variables.rb
@@ -3,3 +3,7 @@
 #Write a program called name.rb that asks the user to type in their name and then prints out a greeting message with their name included.
 
 #Write a program called age.rb that asks a user how old they are and then tells them how old they will be in 10, 20, 30 and 40 years. Below is the output for someone 20 years old.
+
+#Add another section onto name.rb that prints the name of the user 10 times. You must do this without explicitly writing the puts method 10 times in a row. Hint: you can use the times method to do something repeatedly.
+
+#Modify name.rb again so that it first asks the user for their first name, saves it into a variable, and then does the same for the last name. Then outputs their full name all at once.
