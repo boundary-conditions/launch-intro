70e1a898b44f1202004dc597157de2b982b53efc
Charles Keenan <Keenan.Charlese@gmail.com>
2020-06-08 11:06:37 -0300

    Changed name program to gets both first and last name, and the concatenate them together

--- variables/name.rb
+++ variables/name.rb
@@ -1,8 +1,12 @@
-puts "Hello, I am Mac. What's your name?"
-name = gets.chomp
+puts "Hello, I am Mac. What's your first name?"
+first_name = gets.chomp
+puts "I like that, what is your last name?"
+last_name = gets.chomp
+
+name = "#{first_name} #{last_name}"
 
 puts "Hello #{name}, it's nice to meet you..."
 
 puts "I really like your name #{name}, I just want to say it over and over and over and over...."
-10.times { print "#{name}" }
+10.times { print "#{name}, " }
 puts "\n There....that's better #{name}."
