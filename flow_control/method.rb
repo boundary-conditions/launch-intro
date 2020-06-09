de9094e05751eb115ee9037b2fea292b3d9ebb69
Charles Keenan <Keenan.Charlese@gmail.com>
2020-06-09 09:56:36 -0300

    if elsif method for reporting on numbers

--- flow_control/method.rb
+++ flow_control/method.rb
@@ -7,3 +7,14 @@ def string_method(string)
   end
 end
 
+puts "Give me a number!"
+user_number = gets.chomp.to_i
+if user_number < 0
+  puts "Number is less than 0"
+elsif user_number < 51
+  puts "Number is between 0 and 50!"
+elsif user_number < 101
+  puts "Number is between 51 and 100!"   
+ else
+  puts "Number is over 100!"
+end
