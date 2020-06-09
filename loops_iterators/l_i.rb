0baf67a34e753008dffc8c75eee3c51cdbf5a557
Charles Keenan <Keenan.Charlese@gmail.com>
2020-06-09 13:12:26 -0300

    Countdown method using recursion

--- loops_iterators/l_i.rb
+++ loops_iterators/l_i.rb
@@ -4,3 +4,16 @@ while user_input != "STOP"
   puts "What would you like to do?"
   user_input = gets.chomp
 end
+
+def countdown(i)
+  if i == 0
+  else
+    i -= 1
+    puts i
+    countdown(i)
+  end
+end
+
+puts "What number do you want to countdown from?"
+user_integer = gets.chomp.to_i
+countdown(user_integer)
--- loops_iterators/loops_iterators.md
+++ loops_iterators/loops_iterators.md
@@ -1,1 +1,3 @@
 #Write a while loop that takes input from the user, performs an action, and only stops when the user types "STOP". Each loop can get info from the user.
+
+#Write a method that counts down to zero using recursion.
