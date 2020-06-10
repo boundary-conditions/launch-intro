5c2989e8b507aa743c50f3630bc031d1577032f0
Charles Keenan <Keenan.Charlese@gmail.com>
2020-06-10 12:36:41 -0300

    How to use merge and merge! including examples added to hash_exercises.rb

--- hash/hash_exercises.rb
+++ hash/hash_exercises.rb
@@ -7,4 +7,21 @@ family = {  uncles: ["bob", "joe", "steve"],
 
 immediate_family = family.select { |k,v| k == :brothers || k == :sisters }
 
-#2
+#2 merge! is destructive, as in it modifies the hash its called on, whereas merge outputs a new hash.
+h1 = {a: 1, b: 2, c: 3, d: 4}
+h2 = {e: 5, f: 6, g: 7, h: 8}
+
+def non_destructive(h1,h2)
+  puts "This is what happens when you use regular merge"
+  puts h1.merge(h2)
+  puts "h1 remains the same, as merge outputs a new hash without modifying the original. \n#{h1}"
+  puts "h2 remains unchanged \n#{h2}"
+end
+
+def destructive(h1,h2)
+  puts "This is what happens when you use merge!"
+  puts h1.merge!(h2)
+  puts "This is the new version of h1 \n#{h1}"
+  puts "h2 remains unchanged: \n#{h2}"
+end
+
