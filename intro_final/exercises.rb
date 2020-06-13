10e800cc2622c1ab8dabb858134f829a10c4e324
Charles Keenan <Keenan.Charlese@gmail.com>
2020-06-13 13:16:20 -0300

    Adding arrays to hashes using an if else statement

--- intro_final/exercises.rb
+++ intro_final/exercises.rb
@@ -36,3 +36,51 @@ arr.uniq! #uniq removes duplicates, with bang it modifies array, without returns
 
 #What's the major difference between an Array and a Hash?
 #An array keeps items organized by index number, in order, a hash organizes things using descriptive pairs.
+
+#Create a Hash, with one key-value pair, using both Ruby syntax styles.
+literal_hash = {:l_key => "value"}
+hash = Hash.new
+hash[:key] = "value"
+new_hash = {n_key: "value"}
+
+#Suppose you have a hash h = {a:1, b:2, c:3, d:4}
+#1. Get the value of key `:b`.
+h[:b]
+#2 Add to this hash the key:value pair `{e:5}`
+h[:e] = 5
+#3 Remove all key:value pairs whose value is less than 3.5
+h.each do |k,v|
+  if v < 3.5
+    h.delete(k)
+  end
+end
+
+#Can hash values be arrays? Can you have an array of hashes? (give examples)
+arrays_as_values = {:hash1 => [1,2,3,4,5]}
+#yes arrays can be used as a value in a hash key:value pair
+hash_arr = [{a: 2, b: 3},{c: 4, d: 5}] #Yes hashes can be stored in an array
+hash_arr[0][:b] #to access the hash you use array index number followed by the key.
+
+#Given the following data structures. Write a program that copies the information 
+#from the array into the empty hash that applies to the correct person.
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+
+contact_data.each do |a|
+  a.each do |b|
+    if b.include?("joe")
+      contacts["Joe Smith"] = {:email => a[0],:address => a[1],:phone => a[2]}
+    else
+      contacts["Sally Johnson"] = {:email => a[0],:address => a[1],:phone => a[2]}
+    end
+  end 
+end
+
+
+
+
+  
+  
+
