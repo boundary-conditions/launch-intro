8d219b8a07e22825a4654520c5eb314b2f3be700
Charles Keenan <Keenan.Charlese@gmail.com>
2020-06-13 20:01:21 -0300

    Finished the last couple exercises

--- intro_final/exercises.rb
+++ intro_final/exercises.rb
@@ -105,14 +105,49 @@ a = ['white snow', 'winter wonderland', 'melting ice',
 new_array = a.map { |s| s.split(" ") }
 new_array.flatten!
 
+#What will the following program output?
+
+hash1 = {shoes: "nike", "hat" => "adidas", :hoodie => true}
+hash2 = {"hat" => "adidas", :shoes => "nike", hoodie: true}
+
+if hash1 == hash2
+  puts "These hashes are the same!"
+else
+  puts "These hashes are not the same!"
+end
+
+#Challenge: In exercise 11, we manually set the contacts hash values one by one. Now, programmatically loop or iterate over the contacts hash from exercise 11, and populate the associated data from the contact_data array. Hint: you will probably need to iterate over ([:email, :address, :phone]), and some helpful methods might be the Array shift and first methods.
+
+#Note that this exercise is only concerned with dealing with 1 entry in the contacts hash, like this:
+
+contact_data = ["joe@email.com", "123 Main st.", "555-123-4567"]
+contacts = {"Joe Smith" => {}}
+
+arr_pop = [:email, :address, :phone]
+
+arr_pop.each do |a|
+  contacts["Joe Smith"][a] = contact_data.shift
+end
 
 
 
+# My first try at the previous problem
+#contacts.each do |k,v|
+#    v[arr_pop][0] = contact_data[0]
+#    arr_pop.shift
+#    contact_data.shift
+#end
 
+#As a bonus, see if you can figure out how to make it work with multiple entries in the contacts hash.
 
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+fields = [:email, :address, :phone]
 
-
-
-  
-  
+contacts.each_with_index do |(name, hash), idx| #Enumerator, idx iterates from 0++ with each 'each' instance
+  fields.each do |field|
+    hash[field] = contact_data[idx].shift #here contact_data[idx] starts at 0 for the first each instance on contacts, and moves to [1] for the second.
+  end
+end
 
